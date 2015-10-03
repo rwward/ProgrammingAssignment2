2e9667ff2a570880123a4b61d6543226ea7491ab
Robert <robertwessonward@gmail.com>
2015-10-02 22:31:35 -0400

    both functions written
    help i'm stuck in a text editor factory

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -12,4 +22,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
+
+m <- matrix(1:4, nrow = 2, ncol = 2)
+m
+solve(m)
+
+cachem <- makeCacheMatrix(matrix())
+for(i in 1:2) print(cacheSolve(cachem))
+
+cacheSolve(cachem)
